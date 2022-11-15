38f58a6e5aa128bb915e65b658afe6718f6d2e6b
karicotiza <Kur7umerovD@Gmail.com>
2022-11-15 18:41:02 +0300

    fixed config

--- Код/config.py
+++ Код/config.py
@@ -1,4 +1,4 @@
 mini_lm_path = "C://Storage//Net//minilm-uncased-squad2"
 wmt19_ru_en = "C://Storage//Net//wmt19-ru-en"
 opus_mt_en_ru = "C://Storage//Net//opus-mt-en-ru"
-question = "What does this company produce ?"
+question = "What industry does this company belong to ?"
